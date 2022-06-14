e2036cf7cff5cc034db61dcc0acb448c1aaa9934
Zhaoliang <zhz03@g.ucla.edu>
2022-06-14 01:55:48 -0700

    add download_yml.py

---
+++ opencood/hypes_yaml/download_yaml.py
@@ -0,0 +1,6 @@
+from opencood.utils.common_utils import download_googledrive_zipmodel_gdown
+
+if __name__ == '__main__':
+    yaml_googledrive_url = 'https://drive.google.com/uc?export=download&id=1yVaBbTU16b1oyi1DfCVT6wvMu31Ydnv4' 
+    filePath = './opencood/hypes_yaml/yamlfiles.zip'
+    download_googledrive_zipmodel_gdown(yaml_googledrive_url,filePath,remove_zip=True)
