110fdfc12234606ccc51aff14713b552a00b333f
Zhaoliang <zhz03@g.ucla.edu>
2022-06-14 07:02:22 -0700

    add show version

--- opencood/cood.py
+++ opencood/cood.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-import site
+from opencood.version import __version__
 
 def test_parser():
     parser = argparse.ArgumentParser(description="opencood command")
@@ -8,6 +8,9 @@ def test_parser():
     parser.add_argument('--model_dir', type=str, required=True,
                         help='Continued training path')
     """
+    parser.add_argument("-V","--version",action='store_true',
+                        help="display version",
+                    )
     parser.add_argument('--model', type=str,
                         default='None',
                         help="Your model name or use [all] to download all models at once")
@@ -36,14 +39,20 @@ def main():
     print(c_path)
     os.chdir(c_path)
     print("Current Working Directory After Change:" , os.getcwd()) 
+    
+    if opt.version:
+        print("opencoodx version:",__version__)
+        
     if opt.yaml:        
         # cmd = 'python ' + ab_path + '/hypes_yaml/download_yaml.py'
         cmd = 'python ' + c_path + '/hypes_yaml/download_yaml.py'
         os.system(cmd)
         print('Yaml files have been downloaded!')
+    else:
+        print('Reminder: You need to download yaml files before you run the code!')
     
     if opt.model == 'None':
-        print('Warning: You need to download trained model before you run the code!')
+        print('Reminder: You need to download trained model before you run the code!')
     else:
         cmd = 'python ' + c_path + '/model_dir/download_models.py --model ' + opt.model
         os.system(cmd)
