6c15112c98708ae425238392099c61522df1cc6f
Zhaoliang <zhz03@g.ucla.edu>
2022-06-14 14:21:58 -0700

    return to previous

--- opencood/utils/setup.py
+++ opencood/utils/setup.py
@@ -1,6 +1,16 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
+import os
+
+"""
+print('Getting into utils/setup.py')
+c_path = os.path.abspath(os.path.dirname(__file__)) # string
+print("Current Working Directory:" , os.getcwd())
+print(c_path)
+os.chdir(c_path)
+print("Current Working Directory After Change:" , os.getcwd()) 
+"""
 setup(
     name='box overlaps',
     ext_modules=cythonize('opencood/utils/box_overlaps.pyx'),
