6f338a22813a073ea3732e0e75da4eecfa96d84a
Zhaoliang <zhz03@g.ucla.edu>
2022-06-15 23:31:38 -0700

    change setup path to ads path

--- opencood/utils/setup.py
+++ opencood/utils/setup.py
@@ -3,16 +3,24 @@ from Cython.Build import cythonize
 import numpy
 import os
 
-"""
+
 print('Getting into utils/setup.py')
 c_path = os.path.abspath(os.path.dirname(__file__)) # string
+
 print("Current Working Directory:" , os.getcwd())
 print(c_path)
-os.chdir(c_path)
+
+upper_path = os.path.dirname(c_path)
+root_path = os.path.abspath(os.path.dirname(upper_path)) # string
+print(root_path)
+os.chdir(root_path)
+
 print("Current Working Directory After Change:" , os.getcwd()) 
-"""
+
+# ext_modules=cythonize('opencood/utils/box_overlaps.pyx'),
+
 setup(
     name='box overlaps',
-    ext_modules=cythonize('opencood/utils/box_overlaps.pyx'),
+    ext_modules=cythonize(c_path + '/box_overlaps.pyx'),
     include_dirs=[numpy.get_include()]
 )
